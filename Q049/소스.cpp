bd4ca20a8cd99a5dfb411c2a88355a9558776035
bkzero1 <84488611+bkzero1@users.noreply.github.com>
2026-09-08 17:28:30 +0900

    Q049 refactor: 중복 검사를 일괄 처리하도록 변경
    각 합을 생성할 때마다 find로 중복을 검사하던 방식을 변경
    
    모든 합을 vector에 먼저 저장한 뒤 sort, unique, erase를 사용해 중복을 마지막에 한꺼번에 정리하도록 리팩터링

--- Q049/소스.cpp
+++ Q049/소스.cpp
@@ -6,23 +6,32 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <set>
 
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
-    std::set<int> uniqueSums;
+    vector<int> answer;
 
     for (int i = 0; i + 1 < numbers.size(); ++i)
     {
         for (int j = i + 1; j < numbers.size(); ++j)
         {
-            uniqueSums.insert(numbers[i] + numbers[j]);
+            int sum = numbers[i] + numbers[j];
+    
+            // 중복허용하게 저장하고 나중에 정리
+            answer.push_back(sum);
         }
     }
 
-    // 중복이 제거되고 오름차순으로 정렬된 set의 원소를 결과 벡터로 복사
-    vector<int> answer(uniqueSums.begin(), uniqueSums.end());
+    // unique는 연속된 중복만 정리하므로, 같은 값이 서로 붙도록 합들을 오름차순으로 정렬
+    std::sort(answer.begin(), answer.end());
+
+    // 중복을 정리한 뒤 새로운 논리적 끝을 받음
+    auto newEnd = std::unique(answer.begin(), answer.end());
+
+    // 논리적 끝 이후의 원소를 벡터에서 실제로 제거
+    answer.erase(newEnd, answer.end());
+
     return answer;
 }
 
